c9ba36a8a6207a96d196e94cd63afb6d18213c89
Deepesh Ojha <141874764+deepesh0jha@users.noreply.github.com>
2023-11-24 18:25:40 +0530

    update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML ="<h1>not just stupid, very stupid</h1>";
+  question.innerHTML ="<h1>peppa pig</h1>";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
